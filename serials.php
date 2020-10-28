d15c5be09ef7b95dc41e3ad2ec85924aea4ee992
Ruslan Zamaliev <62461403+Luslan2k@users.noreply.github.com>
2020-10-29 02:05:15 +0300

    1 залива сайта
    Пока не работают ПХП
    Нет авторизации
    Не заполнены сериалы
    и не созданы

---
+++ serials.php
@@ -0,0 +1,274 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <title>Киномонстр Bt - портал о кино</title>
+  </head>
+  <body>
+    
+
+      <div class="container-fluid">  <!-----ХЕДЕР-------> 
+
+        <div class="row">
+
+            <nav role="navigation" class="navbar navbar-inverse">
+
+              <div class="container">
+
+                <div class="navbar-header header">
+
+                  <div class="container">
+
+                    <div class="row">
+
+                      <div class="call-lg-12">
+                        <h1> <a href="index.html">КиноМонстр</a></h1>
+                        <p>Кино - Наша страсть!</p>
+                      </div>
+
+                    </div>
+
+                  </div>
+                  
+                  <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle"> <!--Меню навигации-->
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+
+                </div>
+
+                <div id="navbarCollapse" class="collapse navbar-collapse navbar-right">
+                  
+                  <ul class="nav nav-pills">
+                    <li><a href="index.html">Главная</a> </li>
+                    <li><a href="films.html">Фильмы</a> </li>
+                    <li class="active"><a href="serials.php">Сериал</a></li>
+                    <li><a href="rating.html">Рейтенг фильмов</a></li>
+                    <li><a href="contact.html">Контакты</a> </li>
+                  </ul>
+
+                </div>
+
+              </div>
+
+            </nav>
+
+        </div>
+
+      </div>  
+    
+      <div class="wrapper">
+        
+        <div class="container">
+
+          <div class="row">
+              
+            <div class="col-lg-9 col-lg-push-3"> <!--Правый блок страницы-->
+
+              
+              <h1>Сериалы</h1>
+              <hr>
+              
+              <div class="row">              <!--Правый блок страницы ВрекингВед-->
+                <div class="well clearfix">
+                  <div class="col-lg-3 col-md-2 text-center">
+                    <img class="img-thumbnail" src="assets/img/breakingbad.png" alt="Во все тяжки">
+                    <p>Во все тяжки</p>
+                  </div>
+
+                  <div class="col-lg-9 col-md-10">
+                    <p>Школьный учитель химии Уолтер Уайт узнаёт, что болен раком лёгких. Учитывая сложное финансовое состояние дел семьи, а также перспективы, Уолтер решает заняться изготовлением метамфетамина. Для этого он привлекает своего бывшего ученика Джесси Пинкмана, когда-то исключённого из школы при активном содействии Уайта. Пинкман сам занимался варкой мета, но накануне, в ходе рейда УБН, он лишился подельника и лаборатории.</p>
+                  </div>
+
+                  <div class="col-lg-12">
+                    <a href="showbb.php" class="btn btn-lg btn-warning pull-right"> Подробнее</a>
+                  </div>
+                </div>
+              </div> 
+
+
+              <div class="row">              <!--Правый блок страницы Ходячие мертвецы-->
+                <div class="well clearfix">
+                  <div class="col-lg-3 col-md-2 text-center">
+                    <img class="img-thumbnail" src="assets/img/dead.png" alt="Ходячие мертвецы">
+                    <p>Ходячие мертвецы</p>
+                  </div>
+
+                  <div class="col-lg-9 col-md-10">
+                    <p>Сериал рассказывает историю жизни семьи шерифа после того, как «зомби» - эпидемия апокалиптических масштабов захлестнула земной шар. Шериф Рик Граймс путешествует со своей семьей и небольшой группой выживших в поисках безопасного места для жизни. Но постоянный страх смерти каждый день приносит тяжелые потери, заставляя героев почувствовать глубины человеческой жестокости. Рик пытается спасти свою семью, и открывает для себя, что всепоглощающий страх тех, кто выжил, может быть опаснее бессмысленных мертвецов, бродящих по земле.</p>
+                  </div>
+
+                  <div class="col-lg-12">
+                    <a href="showdead.php" class="btn btn-lg btn-warning pull-right"> Подробнее</a>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="row">              <!--Правый блок страницы Силиконовая долина-->
+                <div class="well clearfix">
+                  <div class="col-lg-3 col-md-2 text-center">
+                    <img class="img-thumbnail" src="assets/img/silicon.png" alt="Кремниевая долина">
+                    <p>Силиконовая долина</p>
+                  </div>
+
+                  <div class="col-lg-9 col-md-10">
+                    <p>История о группе гиков, готовящих к запуску собственные стартапы в высокотехнологичном центре Сан-Франциско. Главные герои сериала бесплатно проживают в доме местного миллионера, но взамен им придётся отдать по 10% прибыли от будущих проектов.</p>
+                  </div>
+
+                  <div class="col-lg-12">
+                    <a href="showsilcon.php" class="btn btn-lg btn-warning pull-right"> Подробнее</a>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="row">              <!--Правый блок страницы Секретные материалы-->
+                <div class="well clearfix">
+                  <div class="col-lg-3 col-md-2 text-center">
+                    <img class="img-thumbnail" src="assets/img/xfiles.png" alt="Секретные материалы">
+                    <p>Секретные материалы</p>
+                  </div>
+
+                  <div class="col-lg-9 col-md-10">
+                    <p>Агентам ФБР Дане Скалли и Фоксу Малдеру поручают работу над проектом «Секретные материалы». Это архив нераскрытых дел, связанных с паранормальными явлениями. Малдер верит в пришельцев и пытается убедить скептика Скалли, что не всё поддаётся разумному объяснению. Постепенно взаимное недоверие перерастает в дружбу и даже в более глубокое чувство.</p>
+                  </div>
+
+                  <div class="col-lg-12">
+                    <a href="showxfiles.html" class="btn btn-lg btn-warning pull-right"> Подробнее</a>
+                  </div>
+                </div>
+              </div>
+
+
+
+
+              <div class="margin-8"></div>
+
+            </div>
+            
+            <div class="col-lg-3 col-lg-pull-9"> <!--Левый блок страницы-->
+              
+             <div class="panel panel-info hidden-xs">
+              <div class="panel-heading"><div class="sidebar-header">Поиск</div></div>
+              <div class="panel-body">
+                <form role="search">
+                  <div class="form-group">
+                    <div class="input-group">
+                      <input type="search" class="form-control input-lg" placeholder="Ваш запрос">
+                      <div class="input-group-btn">
+                        <button class="btn btn-default btn-lg" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+            <div class="panel panel-info">  <!--Вход-->
+              <div class="panel-heading"><div class="sidebar-header">Вход</div></div>
+                <div class="panel-body">
+                  <form role="form">
+                    <div class="form-group">
+                      <input type="text" class="form-control input-lg" placeholder="Логин">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control input-lg" placeholder="Пароль">
+                    </div>
+                    <button type="submit" class="btn btn-warning pull-right">вход</button>
+                  </form>
+                </div>
+            </div>
+
+             <div class="panel panel-info"> <!--Новости-->
+              <div class="panel-heading"><div class="sidebar-header">Новости</div></div>
+                <div class="panel-body">
+                  <p class="news-side">09.10.2020</p>
+                  <p class="news-side">Готовимся к диплому!</p>
+              </div>
+            </div>
+
+             <div class="panel panel-info">  <!--Рейтинг-->
+              <div class="panel-heading"><div class="sidebar-header">Рейтинг фильмов</div></div>
+                <div class="panel-body">
+
+                  <ul class="list-group">
+                    <li class="list-group-item list-group-warning">
+                      <span class="badge">8.9</span>
+                      <a href="showinter.html">Интерстеллар</a>
+                    </li>
+                    <li class="list-group-item list-group-warning">
+                      <span class="badge">8.7</span>
+                      <a href="showmax.html">Безумный Макс</a>
+                    </li>
+                    <li class="list-group-item list-group-warning">
+                      <span class="badge">8.3</span>
+                      <a href="showmatrix.html">Матрица</a>
+                    </li>
+                    <li class="list-group-item list-group-warning">
+                      <span class="badge">8.1</span>
+                      <a href="showcloud.html">Облачный атлас</a>
+                    </li>
+                    <li class="list-group-item list-group-warning">
+                      <span class="badge">0</span>
+                      <a href="#">Coming soon...</a>
+                    </li>
+                    <li class="list-group-item list-group-warning">
+                      <span class="badge">0</span>
+                      <a href="#">Coming soon...</a>
+                    </li>
+                    <li class="list-group-item list-group-warning">
+                      <span class="badge">0</span>
+                      <a href="#">Coming soon...</a>
+                    </li>
+                    <li class="list-group-item list-group-warning">
+                      <span class="badge">0</span>
+                      <a href="#">Coming soon...</a>
+                    </li>
+                  </ul>
+            
+
+              </div>
+            </div>
+
+
+
+            </div>
+          </div>
+        </div>
+        
+        <div class="clear">
+          
+        </div>
+
+      </div>
+
+
+      <footer>   <!------------ФУТЕР---------------->
+        
+        <div class="container">
+          <p class="text-center"> <a href="https://vk.com/lal1n">Руслан Замалиев</a></p>
+          <p class="text-center">09.10.2020</p>
+        </div>
+
+      </footer>
+
+
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
